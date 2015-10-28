9ab00b20e3a729c513a0de928de44061f084500d
shelbybjensen <shelbybjensen@gmail.com>
2015-10-27 19:44:53 -0600

    completed teamService

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,43 +2,44 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 	this.addNewGame = function(gameObj) {
-		url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
-		if (parseInt("gameObj.homeTeamScore") > parseInt("gameObj.opponentScore")) {
-			gameObj.push = {
-				won: 'true'
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won = true
 			};
 		} else {
-			gameObj.push = {
-				won: 'false'
+			gameObj.won = false
 			};
 		}
 		return $http ({
 			method: 'POST',
-			url: 'https://api.parse.com/1/classes/',
-			data: 'gameObj'
+			url: url,
+			data: gameObj
 		})
 	}
 
 	this.getTeamData = function(team) {
 		var dfd = $q.defer();
-		url = 'https://api.parse.com/1/classes/' + team;
+		var url = 'https://api.parse.com/1/classes/' + team;
 		$http ({
 			method: 'GET',
-			url: 'https://api.parse.com/1/classes/' + team
+			url: url
 		}).then(function(data) {
 			var results = data.data.results;
 			var wins = 0;
 			var losses = 0;
 			for (var i = 0; i < results.length; i++) {
-				if (won = true) {
-					won[i++];
+				if (results[i].won = true) {
+					wins++;
 				}else {
-					won[i--];
-				}
-			}
-		})
+					losses++;
+				};
+			};
+			results['wins'] = wins;
+			results['losses'] = losses;
+			dfd.resolve(reuslts);
+		});
 
 		return dfd.promise;
-	}
+	};
 
 });
